ed8bccb99f494272699eed12ff406fc9ec574803
Collin Geersen <collingeersen.git@gmail.com>
2023-03-23 03:23:27 -0400

    add type linting to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
+    extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
     plugins: ['@typescript-eslint'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: "../tsconfig.json",
+    },
     root: true,
   };
