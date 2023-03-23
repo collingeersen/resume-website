169105dce829c2dc83a91b88c1427a54c795f355
Collin Geersen <collingeersen.git@gmail.com>
2023-03-23 04:11:04 -0400

    fix: correct files tracked by eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ],
     plugins: ['@typescript-eslint'],
     parser: '@typescript-eslint/parser',
+    overrides: {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+    },
     parserOptions: {
       project: true,
-      tsconfigRootDir: "../tsconfig.json",
+      tsconfigRootDir: "tsconfig.json",
     },
     root: true,
   };
