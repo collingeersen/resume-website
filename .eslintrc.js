6b3967e70490a78ea7032823994699a4d6ecfcc5
Collin Geersen <collingeersen.git@gmail.com>
2023-03-23 04:14:59 -0400

    format lint files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    plugins: ['@typescript-eslint'],
-    parser: '@typescript-eslint/parser',
-    overrides: {
-      files: ['*.ts', '*.tsx'],
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      ],
-    },
-    parserOptions: {
-      project: true,
-      tsconfigRootDir: "tsconfig.json",
-    },
-    root: true,
-  };
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  overrides: {
+    files: ['*.ts', '*.tsx'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+  },
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: "tsconfig.json",
+  },
+  root: true,
+};
