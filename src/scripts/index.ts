4a621897e48d2e996a41de014f7a9b74ec76a54d
Collin Geersen <collingeersen.git@gmail.com>
2023-04-15 04:50:51 -0400

    fix: add comments for ts file

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,14 +1,19 @@
+//Selectors that fire the initial event
 const btnHamburger = document.getElementsByClassName("nav-bar__btn--hamburger")[0];
 const btnExit = document.getElementsByClassName("nav-bar__dropdown__btn--exit")[0];
 const mainExit = document.getElementsByClassName("main")[0];
 const toggleDisplay = document.querySelectorAll(".--toggle-display");
 
+//Btn click events
 btnHamburger.addEventListener("click", dropDown);
 btnExit.addEventListener("click", dropDown);
 
+//Used for dropdown menu toggle
 function dropDown() {
   const elementDropDown = document.getElementsByClassName("nav-bar__drop-down")[0];
   const main = document.getElementsByClassName("main")[0];
+
+  //Blurs the <main> element and everything within
   if (
     main.classList[1] === undefined ||
     main.classList[1] === "--unblur"
@@ -20,6 +25,7 @@ function dropDown() {
     main.classList.add("--unblur");
   }
 
+  //This makes the dropdown menu either visible or hidden on the btn event
   if (
     elementDropDown.classList[1] === undefined ||
     elementDropDown.classList[1] === "--hidden"
@@ -37,10 +43,15 @@ function dropDown() {
   }
 }
 
+/*
+Iterates through all elements with the --display-none tag and
+adds a click event specific to each sub content for toggle
+*/
 for (let i = 0; i < toggleDisplay.length; i++) {
   toggleDisplay[i].addEventListener("click", fun);
   const sub = document.getElementsByClassName("--" + i + "--display-none")[0];
 
+  //Matches in order from 0 to each of the elements using i as the unique identifier
   function fun() {
     if (
       sub.classList[1] === undefined ||
