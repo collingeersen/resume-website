46c16ecacc1f8ca4287bd97a2bde8a427bf47c28
Collin Geersen <collingeersen.git@gmail.com>
2023-04-19 15:18:17 -0400

    feat: expand/collapse toggle and event refactor

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,10 @@
 //Selectors that fire the initial event
 const btnHamburger = document.getElementsByClassName("nav-bar__btn--hamburger")[0];
-const btnExit = document.getElementsByClassName("nav-bar__dropdown__btn--exit")[0];
+const btnExit = document.getElementsByClassName("nav-bar__drop-down__btn--exit")[0];
 const mainExit = document.getElementsByClassName("main")[0];
 const toggleDisplay = document.querySelectorAll(".--toggle-display");
+const toggleExpandAll = document.getElementsByClassName("nav-bar__drop-down__header__toggle--expand")[0];
+const toggleCollapseAll = document.getElementsByClassName("nav-bar__drop-down__header__toggle--collapse")[0];
 
 //Btn click events
 btnHamburger.addEventListener("click", dropDown);
@@ -15,76 +17,91 @@ function dropDown() {
 
   //Blurs the <main> element and everything within
   if (
-    main.classList[1] === undefined ||
     main.classList[1] === "--unblur"
   ) {
-    main.classList.remove("--unblur");
-    main.classList.add("--blur");
+    main.classList.replace("--unblur", "--blur");
   } else {
-    main.classList.remove("--blur");
-    main.classList.add("--unblur");
+    main.classList.replace("--blur", "--unblur");
   }
 
   //This makes the dropdown menu either visible or hidden on the btn event
   if (
-    elementDropDown.classList[1] === undefined ||
     elementDropDown.classList[1] === "--hidden"
   ) {
-    elementDropDown.classList.remove("--hidden");
-    elementDropDown.classList.add("--visible");
+    elementDropDown.classList.replace("--hidden", "--visible");
   } else {
-    elementDropDown.classList.remove("--visible");
-    elementDropDown.classList.add("--hidden");
+    elementDropDown.classList.replace("--visible", "--hidden");
   }
 
   //Makes it exit when clicked outside of dropdown when active 
-  if (main.classList[1] === "--blur") {
-    mainExit.addEventListener("click", dropDown, { once: true });
+  if (elementDropDown.classList[1] == "--visible") {
+    mainExit.addEventListener("click", dropDown)
+  }
+  //Needed so when exit button is pressed to removes event from main
+  if (elementDropDown.classList[1] == "--hidden") {
+    mainExit.removeEventListener("click", dropDown)
   }
 }
 
 /*
-Iterates through all elements with the --display-none tag and
+Iterates through all elements with the there relevant tags and
 adds a click event specific to each sub content for toggle
 */
 for (let i = 0; i < toggleDisplay.length; i++) {
   toggleDisplay[i].addEventListener("click", subToggle);
+  toggleExpandAll.addEventListener("click", subExpandAll);
+  toggleCollapseAll.addEventListener("click", subCollapseAll);
+
   const sub = document.getElementsByClassName("--" + i + "--display-none")[0];
+  const subArrow = document.getElementsByClassName("--" + i + "--expand-more")[0];
 
   //Matches in order from 0 to each of the elements using i as the unique identifier
   function subToggle() {
     if (
-      sub.classList[1] === undefined ||
       sub.classList[1] === "--" + i + "--display-none"
     ) {
-      sub.classList.remove("--" + i + "--display-none");
-      sub.classList.add("--" + i + "--display-block");
+      sub.classList.replace("--" + i + "--display-none",
+        "--" + i + "--display-block");
     } else {
-      sub.classList.remove("--" + i + "--display-block");
-      sub.classList.add("--" + i + "--display-none");
+      sub.classList.replace("--" + i + "--display-block",
+        "--" + i + "--display-none");
     }
-
-  }
-}
-
-//Iterates through arrow toggle for each subsection with --expand-more tag
-for (let i = 0; i < toggleDisplay.length; i++) {
-  toggleDisplay[i].addEventListener("click", arrowToggle);
-  const sub = document.getElementsByClassName("--" + i + "--expand-more")[0];
-
-  //Matches in order from 0 to each of the elements using i as the unique identifier
-  function arrowToggle() {
     if (
-      sub.classList[1] === undefined ||
-      sub.classList[1] === "--" + i + "--expand-more"
+      subArrow.classList[1] === undefined ||
+      subArrow.classList[1] === "--" + i + "--expand-more"
     ) {
-      sub.classList.remove("--" + i + "--expand-more");
-      sub.classList.add("--" + i + "--expand-less");
+      subArrow.classList.replace("--" + i + "--expand-more",
+        "--" + i + "--expand-less");
     } else {
-      sub.classList.remove("--" + i + "--expand-less");
-      sub.classList.add("--" + i + "--expand-more");
+      subArrow.classList.replace("--" + i + "--expand-less",
+        "--" + i + "--expand-more");
     }
+  }
 
+  function subExpandAll() {
+    if (sub.classList[1] === "--display-none" || "--display-block") {
+      sub.classList.replace("--" + i + "--display-none",
+        "--" + i + "--display-block");
+      subArrow.classList.replace("--" + i + "--expand-more",
+        "--" + i + "--expand-less");
+    }
+    /*
+    if(sub.classList[i] === "--display-block"){
+      toggleExpandAll.classList.replace("--active", "--nonactive")
+    }*/
+  }
+
+  function subCollapseAll() {
+    if (sub.classList[1] === "--display-none" || "--display-block") {
+      sub.classList.replace("--" + i + "--display-block",
+        "--" + i + "--display-none");
+      subArrow.classList.replace("--" + i + "--expand-less",
+        "--" + i + "--expand-more");
+    }
+    /*
+    if(sub.classList[i] !== "--display-none"){
+      toggleCollapseAll.classList.replace("--nonactive", "--active")
+    }*/
   }
 }
 
