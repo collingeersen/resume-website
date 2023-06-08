7fec352d48fe040f70425142c571b4cfb2444687
Collin Geersen <collingeersen.git@gmail.com>
2023-06-07 20:26:17 -0400

    feat: change for dropdown overlay and scroll

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -13,7 +13,7 @@ btnHamburger.addEventListener("click", dropDown);
 btnExit.addEventListener("click", dropDown);
 
 document.addEventListener("scroll", (event) => {
-  if (window.scrollY > 80){
+  if (window.scrollY > 50) {
     navMini.classList.replace("nav-bar--default", "nav-bar--mini");
   } else {
     navMini.classList.replace("nav-bar--mini", "nav-bar--default");
@@ -40,29 +40,27 @@ function dropDown() {
   const elementDropDown = document.getElementsByClassName("nav-bar__drop-down")[0];
   const main = document.getElementsByClassName("main")[0];
   const footer = document.getElementsByClassName("footer")[0];
+  const body = document.getElementsByTagName("body");
 
   //Blurs the <main> element and everything within
-  if (
-    main.classList[1] === "--unblur"
-  ) {
-    main.classList.replace("--unblur", "--blur");
+  //Body replace blocks user from scrolling
+  if (main.classList[1] === "--undarken") {
+    main.classList.replace("--undarken", "--darken");
+    body[0].classList.replace("--scroll", "--scroll-block");
   } else {
-    main.classList.replace("--blur", "--unblur");
+    main.classList.replace("--darken", "--undarken");
+    body[0].classList.replace("--scroll-block", "--scroll");
   }
 
   //Allows the footer to also be blurred
-  if (
-    footer.classList[1] === "--unblur"
-  ) {
-    footer.classList.replace("--unblur", "--blur");
+  if (footer.classList[1] === "--undarken") {
+    footer.classList.replace("--undarken", "--darken");
   } else {
-    footer.classList.replace("--blur", "--unblur");
+    footer.classList.replace("--darken", "--undarken");
   }
 
   //This makes the dropdown menu either visible or hidden on the btn event
-  if (
-    elementDropDown.classList[1] === "--hidden"
-  ) {
+  if (elementDropDown.classList[1] === "--hidden") {
     elementDropDown.classList.replace("--hidden", "--visible");
   } else {
     elementDropDown.classList.replace("--visible", "--hidden");
@@ -92,42 +90,29 @@ for (let i = 0; i < toggleDisplay.length; i++) {
 
   //Matches in order from 0 to each of the elements using i as the unique identifier
   function subToggle() {
-    if (
-      sub.classList[1] === "--" + i + "--sub-collapse"
-    ) {
-      sub.classList.replace("--" + i + "--sub-collapse",
-        "--" + i + "--sub-expand");
+    if (sub.classList[1] === "--" + i + "--sub-collapse") {
+      sub.classList.replace("--" + i + "--sub-collapse", "--" + i + "--sub-expand");
     } else {
-      sub.classList.replace("--" + i + "--sub-expand",
-        "--" + i + "--sub-collapse");
+      sub.classList.replace("--" + i + "--sub-expand", "--" + i + "--sub-collapse");
     }
-    if (
-      subArrow.classList[1] === undefined ||
-      subArrow.classList[1] === "--" + i + "--expand-more"
-    ) {
-      subArrow.classList.replace("--" + i + "--expand-more",
-        "--" + i + "--expand-less");
+    if (subArrow.classList[1] === undefined || subArrow.classList[1] === "--" + i + "--expand-more") {
+      subArrow.classList.replace("--" + i + "--expand-more", "--" + i + "--expand-less");
     } else {
-      subArrow.classList.replace("--" + i + "--expand-less",
-        "--" + i + "--expand-more");
+      subArrow.classList.replace("--" + i + "--expand-less", "--" + i + "--expand-more");
     }
   }
 
   function subExpandAll() {
     if (sub.classList[1] === "--sub-collapse" || "--sub-expand") {
-      sub.classList.replace("--" + i + "--sub-collapse",
-        "--" + i + "--sub-expand");
-      subArrow.classList.replace("--" + i + "--expand-more",
-        "--" + i + "--expand-less");
+      sub.classList.replace("--" + i + "--sub-collapse", "--" + i + "--sub-expand");
+      subArrow.classList.replace("--" + i + "--expand-more", "--" + i + "--expand-less");
     }
   }
 
   function subCollapseAll() {
     if (sub.classList[1] === "--sub-collapse" || "--sub-expand") {
-      sub.classList.replace("--" + i + "--sub-expand",
-        "--" + i + "--sub-collapse");
-      subArrow.classList.replace("--" + i + "--expand-less",
-        "--" + i + "--expand-more");
+      sub.classList.replace("--" + i + "--sub-expand", "--" + i + "--sub-collapse");
+      subArrow.classList.replace("--" + i + "--expand-less", "--" + i + "--expand-more");
     }
   }
 }
