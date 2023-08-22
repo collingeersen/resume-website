f3ca82b23c7edbe9f0a2c49dca98e0acab671c1d
Collin Geersen <collingeersen.git@gmail.com>
2023-08-22 02:58:15 -0400

    feat: correct scroll transition to observer api

--- src/scripts/homepage.ts
+++ src/scripts/homepage.ts
@@ -1,24 +1,24 @@
 //Scroll Event for Intro Note
-const introNote = document.getElementsByClassName("intro__summary__note")[0];
-document.addEventListener("scroll", introNoteScroll, {passive: true});
-function introNoteScroll() {
-  if (window.scrollY > 700) {
-    introNote.classList.add("intro__summary__note--transition");
-    document.removeEventListener("scroll", introNoteScroll);
-  }
-}
+// const introNote = document.getElementsByClassName("intro__summary__note")[0];
+// document.addEventListener("scroll", introNoteScroll, {passive: true});
+// function introNoteScroll() {
+//   if (window.scrollY > 700) {
+//     introNote.classList.add("intro__summary__note--transition");
+//     document.removeEventListener("scroll", introNoteScroll);
+//   }
+// }
 
 //Scroll Event for Section 2 Boxes
-const section2Box = document.getElementsByClassName("section-2__container__box");
-document.addEventListener("scroll", section2BoxScroll, {passive: true});
-function section2BoxScroll() {
-  for (let i = 0; i < section2Box.length; i++) {
-    var scrollAmount = 1300 + (i * 500);
-    if (window.scrollY > scrollAmount) {
-      section2Box[i].classList.add("section-2__container__box--transition");
-    }
-  }
-  if (window.scrollY > scrollAmount) {
-    document.removeEventListener("scroll", section2BoxScroll);
-  }
-}
+// const section2Box = document.getElementsByClassName("section-2__container__box");
+// document.addEventListener("scroll", section2BoxScroll, {passive: true});
+// function section2BoxScroll() {
+//   for (let i = 0; i < section2Box.length; i++) {
+//     var scrollAmount = 1300 + (i * 500);
+//     if (window.scrollY > scrollAmount) {
+//       section2Box[i].classList.add("section-2__container__box--transition");
+//     }
+//   }
+//   if (window.scrollY > scrollAmount) {
+//     document.removeEventListener("scroll", section2BoxScroll);
+//   }
+// }
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,2 +1,28 @@
 import('../components/nav-bar/nav-bar_script').then();
 import('../components/scroll-to-top/scroll-to-top_script').then();
+
+const observer = new IntersectionObserver ((elements) => {
+    elements.forEach((element) => {
+        //console.log(element);
+        if (element.isIntersecting){
+            element.target.classList.add('scroll--show');
+        } else {
+            element.target.classList.remove('scroll--show');
+        }
+    });
+});
+
+const hideElements = document.querySelectorAll('.scroll--hide');
+hideElements.forEach((e) => observer.observe(e));
+
+const observerOnce = new IntersectionObserver ((elements) => {
+    elements.forEach((element) => {
+        //console.log(element);
+        if (element.isIntersecting){
+            element.target.classList.add('scroll--show--once');
+        }
+    });
+});
+
+const hideElementsOnce = document.querySelectorAll('.scroll--hide--once');
+hideElementsOnce.forEach((e) => observerOnce.observe(e));
