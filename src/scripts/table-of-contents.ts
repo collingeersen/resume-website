eabee061b92df6c62d228069db05c09c2653eaa4
Collin Geersen <collingeersen.git@gmail.com>
2023-09-26 16:40:17 -0400

    feat: add close on link click function

--- src/scripts/table-of-contents.ts
+++ src/scripts/table-of-contents.ts
@@ -1,10 +1,17 @@
 let tocState = document.getElementsByClassName('toc--hide')[0];
 let tocHeading = document.getElementsByClassName('toc__heading')[0];
+let tocLink = document.getElementsByClassName('toc__link');
 
-tocHeading.addEventListener("click", () => {
-    if (tocState.classList[1] === "toc--hide"){
+function tocChange(){
+    if (tocState.classList[1] === "toc--hide") {
         tocState.classList.replace("toc--hide", "toc--show");
-    } else{
+    } else {
         tocState.classList.replace("toc--show", "toc--hide");
     }
-} );
+}
+
+tocHeading.addEventListener("click", tocChange);
+
+for (let i = 0; i < tocLink.length + 1; i++) {
+    tocLink[i].addEventListener("click", tocChange)
+}
